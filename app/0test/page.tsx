2ddfde99928e8be6647485cb57def94bc0bfb21f
neo-ming <ming@ming-neoMacBook.lan>
2026-03-19 10:51:29 -0700

    test Fluid text on /0test

--- app/0test/page.tsx
+++ app/0test/page.tsx
@@ -4,13 +4,21 @@ import Link from "next/link";
 
 const Page = () => {
   return (
-    <a
-      href="/"
-      rel="history"
-      className="hover:opacity-60 font-serifmix tracking-wider"
-    >
-      WANG, 王 SZU-YI 思 懿x
-    </a>
+    <div className="font-serifmix flex min-h-screen bg-background text-foreground pl-1 md:pl-60">
+      {/* Main Content */}
+      <main className="flex-1 px-4 sm:px-6 md:px-12">
+        <p className="text-[clamp(12px,2vw,24px)]">
+          Fluid text
+          <a
+            href="/"
+            rel="history"
+            className="hover:opacity-60 font-serifmix tracking-wider"
+          >
+            WANG, 王 SZU-YI 思 懿x
+          </a>
+        </p>
+      </main>
+    </div>
   );
 };
 
